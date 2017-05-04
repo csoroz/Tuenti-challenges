1b676f91fc1442943ba5de073a486fd1078bd0de
csoroz <csoroz@gmail.com>
2017-05-04 21:49:57 +0200

    Add files via upload

---
+++ 1.hs
@@ -0,0 +1,18 @@
+
+divUp a b = n + if m == 0 then 0 else 1
+  where (n,m) = divMod a b
+
+pizzas :: [Int] -> Int
+pizzas = flip divUp 8 . sum
+
+chop :: Int -> [a] -> [[a]]
+chop n [] = []
+chop n xs = ys : chop n zs where (ys,zs) = splitAt n xs
+
+byLines f = interact $ unlines . f . lines
+
+printCase (i,n) = concat ["Case #",show i,": ",show n]
+
+main = byLines $ map printCase . zip [1..] . map (pizzas . f) . g
+  where g (l:ls) = take t (chop 2 ls) where t = read l
+        f [n,xs] = take (read n) $ map read $ words xs
