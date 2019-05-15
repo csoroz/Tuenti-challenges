34046f47877c5d26ac74085f73b226bae1a48e09
csoroz <csoroz@gmail.com>
2019-05-15 02:01:33 +0200

    Update 9_regex.hs

--- 2019/9_regex.hs
+++ 2019/9_regex.hs
@@ -1,41 +1,24 @@
--- [NOTE] For UTF-8 stdin on Windows cmd: chcp 65001
 {-# LANGUAGE LambdaCase #-}
-import Data.Char
-import Data.List
 import Data.List.Split hiding (oneOf)
+import Data.List
+import Data.Char
 import Data.Maybe
 import RegExp
 
+-- [NOTE] For UTF-8 stdin on Windows cmd: chcp 65001
+
 -- Kanji numerals (一二三四五六七八九十百千万)
 
-alt, str :: [Rexp z] -> Rexp z
-alt = foldr (:|) Phi
-str = foldr (:.) Nil
+data Op = Plus | Minus | Times
 
-oneOf :: [z] -> Rexp z
-oneOf = alt . map Single
+instance Show Op where
+    show Plus  = "+"
+    show Minus = "-"
+    show Times = "*"
 
-splitBy p xs = (takeWhile p xs, dropWhile p xs)
-
-classify :: [Int] -> ([Int],[Int],[Int])
-classify s = (nub v, nub w, reverse y)
-  where (v,z) = splitBy (==1) (sort s)
-        (w,y) = splitBy (<10) z
-
-kanjiRexp :: ([Int],[Int],[Int]) -> Rexp Int
-kanjiRexp (v,w,y) = g u 10000
-                 :. g a 1000
-                 :. g a 100
-                 :. g a 10
-                 :. o u
-                 where
-                    u = oneOf (v ++ w)
-                    a = o (oneOf w)
-                    g a b | elem b y = a :. Single b
-                    g _ _ = Nil
-                    o = (:| Nil)
-
-kanjis = enum . kanjiRexp . classify
+eval Plus  = (+)
+eval Minus = (-)
+eval Times = (*)
 
 kanji :: Char -> Int
 kanji = \case
@@ -61,18 +44,37 @@ kanjiVal = \case
     (u:x:xs) | x > 9 -> g u x xs
     where g u x xs = u*x + kanjiVal xs
 
-data Op = Plus | Minus | Times
+splitBy p xs = (takeWhile p xs, dropWhile p xs)
 
-instance Show Op where
-    show Plus  = "+"
-    show Minus = "-"
-    show Times = "*"
+classify :: [Int] -> ([Int],[Int],[Int])
+classify s = (reverse y, nub w, nub v)
+  where (v,z) = splitBy (==1) (sort s)
+        (w,y) = splitBy (<10) z
 
-eval Plus  = (+)
-eval Minus = (-)
-eval Times = (*)
+-- Regexp
 
-check (o,a,b,c) = (eval o) a b == c
+alt, str :: [Rexp z] -> Rexp z
+alt = foldr (:|) Phi
+str = foldr (:.) Nil
+
+oneOf :: [z] -> Rexp z
+oneOf = alt . map Single
+
+-- tens = reverse [10,100,1000,10000]
+-- kanjiRexp (v,w,y) = str (zipWith g [u,a,a,a] tens) :. o u
+
+kanjiRexp :: ([Int],[Int],[Int]) -> Rexp Int
+kanjiRexp (y,w,v) = g u 10000
+                 :. g a 1000
+                 :. g a 100
+                 :. g a 10
+                 :. o u
+                 where
+                    u = oneOf (v ++ w)
+                    a = o (oneOf w)
+                    g a b | elem b y = a :. Single b
+                    g _ _ = Nil
+                    o = (:| Nil)
 
 solve :: (String,String,String) -> (Op,Int,Int,Int)
 solve (as,bs,cs) = fromJust $ find check $ do
@@ -84,10 +86,13 @@ solve (as,bs,cs) = fromJust $ find check $ do
   where
     matches x = let t = sort x in filter ((t==).sort) (kanjis x)
     combs = map kanjiVal . matches . map kanji
+    kanjis = enum . kanjiRexp . classify
+    check (o,a,b,c) = (eval o) a b == c
 
 byLines f = interact $ unlines . f . lines
 
-showCase (i,(o,a,b,c)) = concat ["Case #",show i,": ", unwords [show a, show o, show b,"=",show c]]
+showCase (i,(o,a,b,c)) = concat ["Case #",show i,": ", unwords [x,u,y,"=",z]]
+                                where [x,y,z] = map show [a,b,c]; u = show o
 
 parse s = (a,b,c)
   where
